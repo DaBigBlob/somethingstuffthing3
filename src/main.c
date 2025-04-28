6c8d5581955a7933e076c6a7f7858b79cee4df40
Person <localboxcrox@gmail.com>
2025-04-28 18:22:57 -0400

    stuff

--- src/main.c
+++ src/main.c
@@ -75,7 +75,8 @@ void spawnThought(AppState* apSt, BOOL once) {
     SIZE tbs;
     while (GetTextExtentPoint32A(hdc, nxt_tht, lstrlenA(nxt_tht), &tbs) == 0);
     ReleaseDC(apSt->thtHwnd, hdc);
-    tbs.cy = frameHeight + captionHeight;
+    tbs.cy += frameHeight + captionHeight +5;
+    tbs.cx += 5;
 
     RECT mr;
     GetWindowRect(apSt->mainHwnd, &mr);
@@ -184,7 +185,7 @@ LRESULT CALLBACK MainWndProc(
             char str[30];
             wsprintfA(
                 str,
-                "V2,XM:%d,YM:%d,CXM:%d,CYM:%d,XT:%d,YT:%d,CXT:%d,CYT:%d",
+                "V3,XM:%d,YM:%d,CXM:%d,CYM:%d,XT:%d,YT:%d,CXT:%d,CYT:%d",
                 mr.left,mr.top,
                 mr.right-mr.left,mr.bottom-mr.top,
                 tr.left,tr.top,
