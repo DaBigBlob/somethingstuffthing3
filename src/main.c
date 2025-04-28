78eb7188b86c6d359ebe410b9147615c8b09a6a4
Person <localboxcrox@gmail.com>
2025-04-28 18:51:41 -0400

    stuff

--- src/main.c
+++ src/main.c
@@ -74,7 +74,7 @@ void spawnThought(AppState* apSt, BOOL once) {
     while (GetTextExtentPoint32A(hdc, nxt_tht, lstrlenA(nxt_tht), &tbs) == 0);
     ReleaseDC(apSt->thtHwnd, hdc);
     tbs.cy = (tbs.cy)*2 + frameWidth + captionHeight;
-    tbs.cx += tbs.cy + frameWidth*2;
+    tbs.cx = tbs.cx + frameWidth*2;
 
     RECT mr;
     GetWindowRect(apSt->mainHwnd, &mr);
@@ -200,7 +200,7 @@ LRESULT CALLBACK MainWndProc(
             char str[30];
             wsprintfA(
                 str,
-                "V8, buttonWidth=%d, frameWidth=%d, captionHeight=%d, tbs.cy=%d, tbs.cx=%d, tcx=%d, tcy=%d",
+                "V9, buttonWidth=%d, frameWidth=%d, captionHeight=%d, tbs.cy=%d, tbs.cx=%d, tcx=%d, tcy=%d",
                 buttonWidth, frameWidth, captionHeight, tbs.cy, tbs.cx, tr.right-tr.left, tr.bottom-tr.top
             );
             MessageBoxA(hWnd, str, "Ding!", MB_OK|MB_ICONEXCLAMATION);
