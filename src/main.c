a97ec349de4a6c58cb7229b5ecf989d5d1f29153
Person <localboxcrox@gmail.com>
2025-04-28 09:33:58 -0400

    stuff

--- src/main.c
+++ src/main.c
@@ -98,8 +98,8 @@ LRESULT CALLBACK MainWndProc(
             ) {
                 int mx = (cx + dx)%(apSt->mainPosDim.cx);
                 int my = (cy + dy)%(apSt->mainPosDim.cy);
-                if (mx < 0) mx += apSt->mainPosDim.cx;
-                if (my < 0) my += apSt->mainPosDim.cy;
+                // if (mx < 0) mx += apSt->mainPosDim.cx;
+                // if (my < 0) my += apSt->mainPosDim.cy;
 
                 SetWindowPos(apSt->thtHwnd, HWND_TOP, mx, my, CW_USEDEFAULT, CW_USEDEFAULT, SWP_SHOWWINDOW|SWP_NOSIZE);
             }
@@ -155,7 +155,7 @@ LRESULT CALLBACK MainWndProc(
             char str[30];
             wsprintfA(
                 str,
-                "V8XM:%d,YM:%d,CXM:%d,CYM:%d,XT:%d,YT:%d,CXT:%d,CYT:%d",
+                "V9XM:%d,YM:%d,CXM:%d,CYM:%d,XT:%d,YT:%d,CXT:%d,CYT:%d",
                 apSt->mainPosDim.x,apSt->mainPosDim.y,
                 apSt->mainPosDim.cx,apSt->mainPosDim.cy,
                 apSt->thtPosDim.x,apSt->thtPosDim.y,
